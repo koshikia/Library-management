7040d8984fc0f8fef9ae69cbb83204afef8c7a0e
vanphuc <vanphuc17504@gmail.com>
2026-01-26 14:06:53 +0700

    Update database table ycdattruoc, phieumuon, ctphieumuon

--- backend/database.sql
+++ backend/database.sql
@@ -1,12 +1,36 @@
 CREATE DATABASE IF NOT EXISTS library_db;
 USE library_db;
 
-CREATE TABLE IF NOT EXISTS book (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255),
-    author VARCHAR(255),
-    year INT
+CREATE TABLE YeuCauDatTruoc (
+    maYeuCau NVARCHAR(50) PRIMARY KEY,
+    maDocGia NVARCHAR(50) NOT NULL, 
+    maDauSach NVARCHAR(50) NOT NULL,
+    ngayDat DATETIME DEFAULT CURRENT_TIMESTAMP, 
+    ngayHetHanCho DATE,
+    trangThai ENUM('DangCho', 'DaDuyet', 'DaHuy') DEFAULT 'DangCho', 
+    
+    FOREIGN KEY (maDocGia) REFERENCES DocGia(maNguoiDung),
+    FOREIGN KEY (maDauSach) REFERENCES DauSach(maDauSach)
 );
 
-INSERT INTO book (title, author, year)
-VALUES ('Book 1', 'Admin', 2026);
+CREATE TABLE PhieuMuon (
+    maPhieu NVARCHAR(50) PRIMARY KEY,
+    maDocGia NVARCHAR(50) NOT NULL,
+    maThuThu NVARCHAR(50) NOT NULL,
+    ngayMuon DATE DEFAULT (CURRENT_DATE), 
+    hanTra DATE NOT NULL,
+    ngayTraThucTe DATE,
+    trangThai ENUM('DangMuon', 'DaTra', 'QuaHan') DEFAULT 'DangMuon',
+    
+    FOREIGN KEY (maDocGia) REFERENCES DocGia(maNguoiDung),
+    FOREIGN KEY (maThuThu) REFERENCES ThuThu(maNguoiDung)
+);
+CREATE TABLE ChiTietPhieuMuon (
+    maPhieu NVARCHAR(50),
+    maVach NVARCHAR(50),
+    tinhTrangKhiMuon NVARCHAR(100),
+    tinhTrangKhiTra NVARCHAR(100),
+    PRIMARY KEY (maPhieu, maVach), -- Khóa chính phức hợp
+    FOREIGN KEY (maPhieu) REFERENCES PhieuMuon(maPhieu),
+    FOREIGN KEY (maVach) REFERENCES BanSaoSach(maVach)
+);
